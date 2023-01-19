20f0925896e2f62bad1ce9c1eb9841e2f2aec45e
Christopher M. Judd <javajudd@gmail.com>
2023-01-18 21:56:13 -0500

    Added a basic map.

---
+++ assets/tilemaps/tileset-grassland-paths.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset-grassland-paths" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+ <image source="tileset-grassland-paths.png" width="364" height="364"/>
+</tileset>
